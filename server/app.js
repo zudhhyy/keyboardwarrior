c9ebf131e7a4325826e44a7370b7b2a223e121f5
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-05-14 14:36:40 +0700

    Boilerplate

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const route = require('./router')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+app.use(route)
+
+http.listen(port, () => {
+    console.log(`This app is listening on port: ${port}`)
+})
