b6bfe243b95fcd252bbcc2b4315b76fb920afba7
zuhdi <zudhy.duddy@gmail.com>
2020-05-14 22:06:36 +0700

    add leaderboard

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
-
+const url = 'http://localhost:3000'
 export default new Vuex.Store({
   state: {
+    datas: []
   },
   mutations: {
+    fetchLeaderboards (state, payload) {
+      state.datas = payload
+    }
   },
   actions: {
+    fetchLeaderboards (context) {
+      axios({
+        method: 'GET',
+        url: `${url}/leaderboard`,
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+        .then((response) => {
+          context.commit('fetchLeaderboards', response.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   },
   modules: {
   }
